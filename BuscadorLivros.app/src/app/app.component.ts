1524ca9d71b51cdb972c1b5b93f5d4b6643782bd
LucasMF1 <lucas.mont.freitas@gmail.com>
2024-09-17 13:23:37 -0300

    paginação

--- BuscadorLivros.app/src/app/app.component.ts
+++ BuscadorLivros.app/src/app/app.component.ts
@@ -7,7 +7,6 @@ import { forkJoin } from 'rxjs';
 import { AuthService } from './auth.service';
 import { FavoritesComponent } from './favorites/favorites.component'; // Importando o FavoritesComponent
 
-
 interface Book {
   title: string;
   author_name?: string[];
@@ -17,7 +16,6 @@ interface Book {
   key?: string;
   isFavorited?: boolean;  // Adicionando a propriedade para indicar se é favoritado
 }
-
 
 @Component({
   selector: 'app-root',
@@ -30,9 +28,17 @@ export class AppComponent implements OnInit {
   title = 'BuscadorLivros.app';
   books: Book[] = [];
   loggedIn = false;
+  totalBooksFound = 0;
   username = '';
   password = '';
   activePage = 'books';
+
+  // Propriedades para controle de busca e paginação
+  query = ''; // Propriedade para armazenar a consulta do usuário
+  searchBy = 'title'; // Valor padrão para o tipo de busca (título ou autor)
+  currentPage = 1; // Página atual
+  totalPages = 1; // Total de páginas disponíveis
+  pageSize = 20;  // Limite de resultados por página
 
   constructor(private http: HttpClient, private router: Router, private authService: AuthService) {}
 
@@ -58,6 +64,14 @@ export class AppComponent implements OnInit {
     }
   }
 
+  // Função chamada quando o usuário muda o tamanho da página
+  onPageSizeChange(event: Event): void {
+    const target = event.target as HTMLSelectElement;
+    const newPageSize = Number(target.value);
+    this.pageSize = newPageSize;  // Atualiza o tamanho da página com o valor selecionado
+    this.search(this.query, this.searchBy, 1);  // Reinicia a busca na página 1 com o novo tamanho
+  }
+
   navigateToFavorites(): void {
     this.activePage = 'favorites';
   }
@@ -77,25 +91,34 @@ export class AppComponent implements OnInit {
     this.router.navigate(['/login']);
   }
 
-  search(query: string, searchBy: string): void {
-    console.log('Query:', query, 'Search By:', searchBy);  // Log para depuração
-    
+  // Função de busca com suporte à paginação
+  search(query: string, searchBy: string, page = 1): void {
+    // Armazena a consulta e o tipo de busca no estado do componente
+    this.query = query;
+    this.searchBy = searchBy;
+  
+    console.log('Query:', query, 'Search By:', searchBy, 'Page Size:', this.pageSize);  // Log para depuração
+  
     if (query && (searchBy === 'title' || searchBy === 'author')) {
-      const apiUrl = `https://openlibrary.org/search.json?${searchBy}=${query}`;
-      this.http.get<{ docs: Book[] }>(apiUrl)
+      const apiUrl = `https://openlibrary.org/search.json?${searchBy}=${query}&page=${page}&limit=${this.pageSize}`;
+      this.http.get<{ docs: Book[], num_found: number }>(apiUrl)
         .subscribe((data) => {
           this.books = data.docs.map(book => ({
             ...book,
             description: book.first_sentence || book.description || 'Descrição não disponível'
           }));
+          
+          this.totalBooksFound = data.num_found;
+          this.totalPages = Math.ceil(data.num_found / this.pageSize);  // Calcula o número total de páginas
+          this.currentPage = page;
   
           // Após obter os livros, verificamos se algum deles já está nos favoritos
           this.checkFavorites();
         });
     } else if (query && searchBy === 'todo') {
-      const titleRequest = this.http.get<{ docs: Book[] }>(`https://openlibrary.org/search.json?title=${query}`);
-      const authorRequest = this.http.get<{ docs: Book[] }>(`https://openlibrary.org/search.json?author=${query}`);
-    
+      const titleRequest = this.http.get<{ docs: Book[] }>(`https://openlibrary.org/search.json?title=${query}&limit=${this.pageSize}`);
+      const authorRequest = this.http.get<{ docs: Book[] }>(`https://openlibrary.org/search.json?author=${query}&limit=${this.pageSize}`);
+  
       forkJoin([titleRequest, authorRequest]).subscribe(([titleData, authorData]) => {
         const combinedBooks = [...titleData.docs, ...authorData.docs];
         this.books = this.removeDuplicates(combinedBooks).map(book => ({
@@ -103,21 +126,37 @@ export class AppComponent implements OnInit {
           description: book.first_sentence || book.description || 'Descrição não disponível'
         }));
   
+        this.totalPages = Math.ceil(combinedBooks.length / this.pageSize);  // Calcula o número total de páginas
+        this.currentPage = page;
+  
         // Verificar se algum livro está favoritado
         this.checkFavorites();
       });
     } else {
       alert('Por favor, insira uma consulta válida.');
     }
+  } 
+
+  // Navegação entre páginas (anterior e próxima)
+  prevPage(): void {
+    if (this.currentPage > 1) {
+      this.search(this.query, this.searchBy, this.currentPage - 1);
+    }
   }
   
+  nextPage(): void {
+    if (this.currentPage < this.totalPages) {
+      this.search(this.query, this.searchBy, this.currentPage + 1);
+    }
+  } 
+
   // Novo método para verificar se os livros estão favoritados
   checkFavorites(): void {
-    const userId = localStorage.getItem('userId');  // Presume-se que o ID do usuário esteja salvo no localStorage
+    const userId = localStorage.getItem('userId');
     if (!userId) {
       return;
     }
-  
+
     this.authService.getFavorites(parseInt(userId, 10)).subscribe(favorites => {
       this.books.forEach(book => {
         const bookId = book.key ? book.key.replace('/works/', '') : book.cover_i?.toString();
@@ -127,7 +166,6 @@ export class AppComponent implements OnInit {
       });
     });
   }
-  
 
   private removeDuplicates(books: Book[]): Book[] {
     const seen = new Set<string>();
@@ -148,152 +186,58 @@ export class AppComponent implements OnInit {
     return 'assets/images/default-cover.jpg';
   }
 
-  addToFavorites(book: Book): void {
-    const ratingInput = prompt('Dê uma avaliação (1-5):');
+  // Propriedades para o modal
+  isModalOpen = false;
+  currentBook: Book | null = null;
+  rating = 1;
+  personalNote = '';
+  tags = '';
 
-    if (ratingInput !== null) {
-      const rating = parseInt(ratingInput, 10);
+  // Abre o modal ao clicar no coração
+  openModal(book: Book): void {
+    this.isModalOpen = true;
+    this.currentBook = book;
+  }
 
-      if (!isNaN(rating) && rating >= 1 && rating <= 5) {
-        const personalNote = prompt('Escreva uma nota pessoal sobre o livro:');
-        const tags = prompt('Adicione algumas tags (separadas por vírgulas):');
+  // Fecha o modal
+  closeModal(): void {
+    this.isModalOpen = false;
+    this.currentBook = null;
+    this.rating = 1;
+    this.personalNote = '';
+    this.tags = '';
+  }
 
-        const userId = localStorage.getItem('userId');
-        if (!userId) {
-          alert('Você precisa estar logado para favoritar um livro.');
-          return;
-        }
-
-        const bookId = book.key ? book.key.replace('/works/', '') : book.cover_i?.toString();
-        if (!bookId || !bookId.trim()) {  // Verificação adicional de valores em branco
-          alert('ID do livro não encontrado.');
-          return;
-        }
-        
-
-        this.authService.addFavorite(parseInt(userId), bookId, personalNote || '', rating, tags || '', book.cover_i)
-          .subscribe(
-            () => {
-              alert(`Livro "${book.title}" foi favoritado com sucesso!`);
-            },
-            () => {
-              alert('Erro ao favoritar o livro.');
-            }
-          );
-      } else {
-        alert('Por favor, insira uma avaliação válida entre 1 e 5.');
+  // Salva o favorito
+  saveFavorite(): void {
+    if (this.currentBook) {
+      const userId = localStorage.getItem('userId');
+      if (!userId) {
+        alert('Você precisa estar logado para favoritar um livro.');
+        return;
       }
-    } else {
-      alert('A ação de favoritar foi cancelada.');
-    }
-  }
 
-  toggleFavorite(book: Book): void {
-    if (!book.isFavorited) {
-      // Marcar como favoritado
-      book.isFavorited = true;
-  
-      const ratingInput = prompt('Dê uma avaliação (1-5):');
-  
-      if (ratingInput !== null) {
-        const rating = parseInt(ratingInput, 10);
-  
-        if (!isNaN(rating) && rating >= 1 && rating <= 5) {
-          const personalNote = prompt('Escreva uma nota pessoal sobre o livro:');
-          const tags = prompt('Adicione algumas tags (separadas por vírgulas):');
-  
-          const userId = localStorage.getItem('userId');
-          if (!userId) {
-            alert('Você precisa estar logado para favoritar um livro.');
-            return;
-          }
-  
-          const bookId = book.key ? book.key.replace('/works/', '') : book.cover_i?.toString();
-          if (!bookId) {
-            alert('ID do livro não encontrado.');
-            return;
-          }
-  
-          this.authService.addFavorite(parseInt(userId), bookId, personalNote || '', rating, tags || '', book.cover_i)
-            .subscribe(
-              () => {
-                alert(`Livro "${book.title}" foi favoritado com sucesso!`);
-              },
-              () => {
-                alert('Erro ao favoritar o livro.');
-              }
-            );
-        } else {
-          alert('Por favor, insira uma avaliação válida entre 1 e 5.');
-        }
-      } else {
-        alert('A ação de favoritar foi cancelada.');
+      const bookId = this.currentBook.key ? this.currentBook.key.replace('/works/', '') : this.currentBook.cover_i?.toString();
+      if (!bookId) {
+        alert('ID do livro não encontrado.');
+        return;
       }
+
+      this.authService.addFavorite(parseInt(userId), bookId, this.personalNote, this.rating, this.tags, this.currentBook.cover_i)
+        .subscribe(() => {
+          alert(`Livro "${this.currentBook?.title}" foi favoritado com sucesso!`);
+
+          if (this.currentBook) {
+            this.currentBook.isFavorited = true;
+          }
+
+          this.closeModal();
+        }, () => {
+          alert('Erro ao favoritar o livro.');
+        });
     }
   }
 
-  // Função para limitar a descrição a 40 palavras
-  truncateDescription(description: string, wordLimit: number): string {
-    const words = description.split(' ');
-    if (words.length > wordLimit) {
-      return words.slice(0, wordLimit).join(' ') + '...';
-    }
-    return description;
-  } 
 
-// Propriedades para o modal
-isModalOpen = false;
-currentBook: Book | null = null;
-rating = 1;
-personalNote = '';
-tags = '';
-
-// Abre o modal ao clicar no coração
-openModal(book: Book): void {
-  this.isModalOpen = true;
-  this.currentBook = book;
-}
-
-// Fecha o modal
-closeModal(): void {
-  this.isModalOpen = false;
-  this.currentBook = null;
-  this.rating = 1;
-  this.personalNote = '';
-  this.tags = '';
-}
-
-// Salva o favorito
-saveFavorite(): void {
-  if (this.currentBook) {
-    const userId = localStorage.getItem('userId');
-    if (!userId) {
-      alert('Você precisa estar logado para favoritar um livro.');
-      return;
-    }
-
-    const bookId = this.currentBook.key ? this.currentBook.key.replace('/works/', '') : this.currentBook.cover_i?.toString();
-    if (!bookId) {
-      alert('ID do livro não encontrado.');
-      return;
-    }
-
-    this.authService.addFavorite(parseInt(userId), bookId, this.personalNote, this.rating, this.tags, this.currentBook.cover_i)
-      .subscribe(() => {
-        alert(`Livro "${this.currentBook?.title}" foi favoritado com sucesso!`);
-
-        // Verifica se currentBook não é nulo antes de alterar a propriedade
-        if (this.currentBook) {
-          this.currentBook.isFavorited = true;
-        }
-
-        // Fecha o modal
-        this.closeModal();
-      }, () => {
-        alert('Erro ao favoritar o livro.');
-      });
-  }
-}
-
-
+  
 }
